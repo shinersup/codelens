73142c0060051f52e46bd8e1b4e53acfff967d3e
shinersup <sjs.shiner@gmail.com>
2026-04-20 18:50:56 -0400

    chore(deploy): pull latest code and rebuild images in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+echo "Pulling latest code..."
+cd /home/ubuntu/codelens
+git pull
+
 echo "Pulling secrets from SSM..."
 aws ssm get-parameters-by-path \
   --path "/codelens/" \
@@ -12,8 +16,7 @@ aws ssm get-parameters-by-path \
     echo "$key=$value"
   done > /home/ubuntu/codelens/.env
 
-echo "Starting containers..."
-cd /home/ubuntu/codelens
-docker-compose pull
+echo "Building and starting containers..."
+docker-compose build
 docker-compose up -d --remove-orphans
 echo "Done."
